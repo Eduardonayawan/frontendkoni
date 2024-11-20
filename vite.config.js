9cd012add3ab41bf943f50edf804ddd3522de9dd
Eduardo Gomez Nayawan JR. <enayawan@gbox.ncf.edu.ph>
2024-11-20 17:28:05 +0800

    sige

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-// vite.config.js
 export default {
-  // Default settings should handle most scenarios
-  build: {
-    // No need to externalize bootstrap unless you're doing so intentionally
-    rollupOptions: {
-      // Only add external libraries here if you're intentionally excluding them from the bundle
-      // external: ['bootstrap']
-    }
-  }
-}
+  plugins: [react()],
+};
